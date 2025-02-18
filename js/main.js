2889501184c3a69e95dbbbfb95aaf3857edf9a34
pehe24kv <pehe24kv@student.ju.se>
2025-02-18 17:33:27 +0100

    hamburger menu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("myhamburgermenu").style.width = "20rem";
+}
+
+function closeNav() {
+  document.getElementById("myhamburgermenu").style.width = "0";
+}
