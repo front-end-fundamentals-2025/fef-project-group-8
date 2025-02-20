4596d72d1217385e20ea43702cec7034194d3870
EmelieKryger <krem24oj@student.ju.se>
2025-02-20 23:01:35 +0100

    shoppingcart 1 step

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-function openNav() {
-  document.getElementById("myhamburgermenu").style.width = "20rem";
+/* JavaScript for hamburgermenu */
+function openHamburgerNav() {
+  document.getElementById("myhamburgermenu").style.display = "block";
+  console.log("hej");
 }
 
-function closeNav() {
-  document.getElementById("myhamburgermenu").style.width = "0";
+function closeHamburgerNav() {
+  document.getElementById("myhamburgermenu").style.display = "none";
+  console.log("hej");
+}
+
+/* JavaScript for cart items */
+function openShoppingNav() {
+  document.getElementById("myshoppingcart").style.display = "block";
+  console.log("hej");
+}
+
+function closeShoppingNav() {
+  document.getElementById("myshoppingcart").style.display = "none";
+  console.log("hej");
 }
