e27b40bf068313c7f264e27c53fe1964d9899074
pehe24kv <pehe24kv@student.ju.se>
2025-02-24 15:31:07 +0100

    Chair information
    Added a select amount

--- js/main.js
+++ js/main.js
@@ -19,3 +19,10 @@ function closeShoppingNav() {
   document.getElementById("myshoppingcart").style.display = "none";
   console.log("hej");
 }
+
+const descriptionButton = document.getElementById("information");
+const textElement = document.getElementById("description");
+
+descriptionButton.addEventListener("click", function () {
+  textElement.classList.toggle("visible");
+});
