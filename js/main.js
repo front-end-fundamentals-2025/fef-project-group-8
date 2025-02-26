8325fd1e9370001cd1cfeec1f7e17b33c2af2a4b
EmelieKryger <krem24oj@student.ju.se>
2025-02-26 11:35:57 +0100

    chair info, buttons
    js with both buttons and fixing picture

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
 /* JavaScript for hamburgermenu */
 function openHamburgerNav() {
   document.getElementById("myhamburgermenu").style.display = "block";
-  console.log("hej");
 }
 
 function closeHamburgerNav() {
   document.getElementById("myhamburgermenu").style.display = "none";
-  console.log("hej");
 }
 
 /* JavaScript for cart items */
 function openShoppingNav() {
   document.getElementById("myshoppingcart").style.display = "block";
-  console.log("hej");
 }
 
 function closeShoppingNav() {
   document.getElementById("myshoppingcart").style.display = "none";
-  console.log("hej");
 }
 
-const descriptionButton = document.getElementById("information");
-const textElement = document.getElementById("description");
+/* Measurments button */
+const measurmentsButton = document.getElementById("information1");
+const textElement1 = document.getElementById("measurments");
+
+measurmentsButton.addEventListener("click", function () {
+  textElement1.classList.toggle("visible");
+});
+
+/* Descrition button */
+const descriptionButton = document.getElementById("information2");
+const textElement2 = document.getElementById("description");
 
 descriptionButton.addEventListener("click", function () {
-  textElement.classList.toggle("visible");
+  textElement2.classList.toggle("visible");
 });
